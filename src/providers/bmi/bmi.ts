d1fb5555861158e0dc4a526f52f122b9593c516d
Rohanee Smurt <604234019@parichat.skru.ac.th>
2018-12-25 11:45:50 +0700

    Edit

---
+++ src/providers/bmi/bmi.ts
@@ -0,0 +1,13 @@
+
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class BmiProvider {
+
+ bmiCalculate(height:number,weight:number){
+   return(weight / height) / height;
+ }
+  
+
+}
